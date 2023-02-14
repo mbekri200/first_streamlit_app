fd79437eb077d60f46f11c0f56fa027f00891ccb
mbekri200 <merybekri5@gmail.com>
2023-02-14 15:18:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -6,3 +8,6 @@ streamlit.text('🍲 Omega3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spnach & Rocket Smoothie')
 streamlit.text('🍗Hard-Boiled  Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
