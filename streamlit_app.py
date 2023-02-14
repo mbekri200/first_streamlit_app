db8b419978cc7f67871eadadbb3e7c72af61ef8f
mbekri200 <merybekri5@gmail.com>
2023-02-14 15:06:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spnach & Rocket Smoothie')
-streamlit.text('Hard-Boiled  Free-Range Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🍲 Omega3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spnach & Rocket Smoothie')
+streamlit.text('🍗Hard-Boiled  Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
