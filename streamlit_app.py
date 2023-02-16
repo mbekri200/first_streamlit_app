93848bba53e97b1eabb78a45056b77afb90a2567
mbekri200 <merybekri5@gmail.com>
2023-02-16 13:56:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,10 @@ fruit_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruit_to_show)
 
 ##########################"
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
